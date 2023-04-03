a7561a889dbf721c2f1bcd0853b81632249a9484
Axu <321axu123@gmail.com>
2023-04-03 22:49:30 +0200

    better tools page

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,12 +1,25 @@
 import Link from "next/link";
 
 export default function Tools() {
+    const tools = {
+        Clp2Img: "/tools/clp2img",
+    };
+
     return (
         <section className='w-[50%] mx-auto flex h-[calc(100vh-5rem)]'>
             <ul className='m-auto w-full'>
-                <li className='p-5 rounded bg-slate-300 hover:cursor-pointer hover:drop-shadow-md hover:underline'>
-                    <Link href='/tools/clp2img'>Clp2Img</Link>
-                </li>
+                {Object.entries(tools).map(([k, v]) => {
+                    return (
+                        <Link
+                            key={k}
+                            className='w-full h-full'
+                            href={v}>
+                            <li className='p-5 rounded bg-slate-300 hover:cursor-pointer hover:drop-shadow-md hover:underline'>
+                                {k}
+                            </li>
+                        </Link>
+                    );
+                })}
             </ul>
         </section>
     );
