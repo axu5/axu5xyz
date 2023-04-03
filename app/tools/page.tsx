9c48e53f312b7b831bf4019c3935000414f72205
Axu <321axu123@gmail.com>
2023-04-03 22:38:57 +0200

    better tools page

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Tools() {
     return (
-        <>
-            <ul>
-                <li>
+        <section className='w-[50%] mx-auto flex h-[calc(100vh-5rem)]'>
+            <ul className='m-auto w-full'>
+                <li className='p-5 rounded bg-slate-300 hover:cursor-pointer hover:drop-shadow-md hover:underline'>
                     <Link href='/tools/clp2img'>Clp2Img</Link>
                 </li>
             </ul>
-        </>
+        </section>
     );
 }
