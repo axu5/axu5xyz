eeac8651f618caa71db1007e0006801be12638ce
Axu <321axu123@gmail.com>
2023-04-21 13:55:20 +0200

    removed duplicate code

--- pages/api/shortr/[short].ts
+++ pages/api/shortr/[short].ts
@@ -6,28 +6,6 @@ export default async function handler(
     res: NextApiResponse
 ) {
     const { short } = req.query;
-
-    if (!short || typeof short !== "string") {
-        res.status(404);
-        return;
-    }
-
-    const dbRes = await db.urlShortener.update({
-        where: {
-            short,
-        },
-        data: {
-            count: {
-                increment: 1,
-            },
-        },
-    });
-
-    if (!dbRes) {
-        res.status(404);
-        return;
-    }
-
-    res.redirect(dbRes.long);
-    return;
+    console.log("LEGACY VERSION DETECTED");
+    res.redirect(`/s/${short}`);
 }
