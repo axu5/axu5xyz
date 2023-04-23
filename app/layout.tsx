28044eb284330210bf4729d8e852afccb08164bd
Axu <321axu123@gmail.com>
2023-04-23 07:47:54 +0200

    better seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,50 @@ import "@/styles/globals.css";
 import getBirthdayParagraph from "@/helpers/getBirthdayParagraph";
 import Link from "next/link";
 import { Analytics } from "@vercel/analytics/react";
+import { cookies } from "next/headers";
 
 type RootLayoutProps = {
     children: ReactNode;
 };
 
+// export const metadata = {
+//     title: "axu5 | dev",
+//     description: getBirthdayParagraph(),
+//     openGraph: {
+//         images: ["https://github.com/axu5.png"],
+//     },
+// };
 export const metadata = {
-    title: "axu5 | dev",
-    description: getBirthdayParagraph(),
+    generator: "Next.js",
+    applicationName: "axu5 | dev",
+    referrer: "origin-when-cross-origin",
+    keywords: ["Portfolio", "Next.js", "Aleksanteri Aho", "axu5"],
+    authors: [{ name: "axu5", url: "https://axu5.xyz" }],
+    // colorScheme: "dark",
+    creator: "Aleksanteri Aho",
+    publisher: "Aleksanteri Aho",
+    // formatDetection: {
+    //     email: false,
+    //     address: false,
+    //     telephone: false,
+    // },
+    openGraph: {
+        title: "Axu5",
+        description: getBirthdayParagraph(),
+        url: "https://axu5.xyz",
+        siteName: "axu5 | dev",
+        images: ["https://github.com/axu5.png"],
+    },
 };
 
 // https://beta.nextjs.org/docs/routing/pages-and-layouts
 // IMPORTANT: MUST INCLUDE HTML, HEAD, AND BODY TAGS
 export default function RootLayout({ children }: RootLayoutProps) {
+    const cookieStore = cookies();
+    const theme = cookieStore.get("theme") || { value: "light" };
+
     return (
-        <html>
+        <html className={theme.value}>
             <head>
                 <link
                     rel='icon'
@@ -27,11 +56,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 />
             </head>
             <body>
-                <nav>
-                    <div className='bg-white w-screen h-20 shadow-xl'>
-                        <div className='flex justify-start items-center w-full h-full px-2 2xl:px-16'>
+                <nav className=''>
+                    <div className='bg-white dark:bg-gray-800 dark:text-white w-full h-20 shadow-xl'>
+                        <div className='flex items-center w-full h-full px-2 2xl:px-16'>
                             <div className='mx-auto flex flex-col md:flex-row md:justify-start w-full md:mx-0'>
-                                <ul className='flex mx-auto md:mx-5'>
+                                <ul className='flex justify-between  mx-auto md:mx-5'>
                                     <li className='nav-link'>
                                         <Link href='/'>Home</Link>
                                     </li>
@@ -55,7 +84,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                         </div>
                     </div>
                 </nav>
-                {children}
+                <div className='dark:bg-slate-700'>{children}</div>
                 <Analytics />
             </body>
         </html>
